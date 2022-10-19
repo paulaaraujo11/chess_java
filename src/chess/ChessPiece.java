95c8413611c25ea5fd43704822014d800b69987c
Paula Araujo <paula.araujo.dev@gmail.com>
2022-10-19 20:32:15 -0300

    implements possibles moviments

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,8 +1,8 @@
 package chess;
 
-import boardgame.Piece;
 import boardgame.Board;
-import chess.Color;
+import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 	
@@ -15,6 +15,11 @@ public abstract class ChessPiece extends Piece {
 
 	public Color getColor() {
 		return color;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
 	}
 
 }
